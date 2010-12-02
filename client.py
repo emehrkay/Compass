36d53aa02728b6a1edfad7bce75d20075fa53dba
Mark Henderson <markhenderson@markhenderson.local>
2010-12-02 18:10:05 -0500

    messing around with the pep8 syntax checker

--- client.py
+++ client.py
@@ -12,24 +12,26 @@ RECORD_ID = '@rid'
 ADMIN = ('admin', 'admin')
 READER = ('reader', 'reader')
 WRITER = ('writer', 'writer')
-    
+
+
 class CompassException(Exception):
     pass
+
 
 class BaseObject(object):
     data = {}
     rid = None
     _immutable = []
-    
+
     def __init__(self, data):
         if data is None:
             data = {}
-            
+
         self.data = data
-    
+
     def save(self):
         pass
-    
+
     def __len__(self):
         return len(self.data)
 
@@ -42,16 +44,16 @@ class BaseObject(object):
     def __getitem__(self, key):
         return self.data[key]
 
-    def __setitem__(self, key, value):        
+    def __setitem__(self, key, value):
         if key in self._immutable:
             raise KeyError('%s is not editable' % (key))
-            
+
         self.data[key] = value
 
     def __delitem__(self, key):
         if key in self._immutable:
             raise KeyError('%s is not editable' % (key))
-            
+
         del self.data[key]
 
 
@@ -60,10 +62,10 @@ class Server(BaseObject):
         'info': '%s/server',
         'disconnect': '%s/disconnect'
     }
-    
+
     def __init__(self, url, username, password):
         super(Server, self).__init__(data=None)
-        
+
         self.url = url
         self.username = username
         self.password = password
@@ -71,7 +73,7 @@ class Server(BaseObject):
         
     def info(self):
         url = self.action['info'] % (self.url)
-        response, content = self.request.get(url=url) 
+        response, content = self.request.get(url=url)
         
         if response.status == 200:
             self.data = json.loads(content)
@@ -87,7 +89,7 @@ class Server(BaseObject):
             
     def database(self, name, credentials=ADMIN, create=False):
         if create:
-            url =  Database.action['post'] % (self.url, name)
+            url = Database.action['post'] % (self.url, name)
             response, content = self.request.post(url=url, data=None)
 
             if response.status == 204:
@@ -102,7 +104,7 @@ class Server(BaseObject):
         
             if response.status == 200:
                 data = json.loads(content)
-                return Database(self.url, name=name, 
+                return Database(self.url, name=name,
                                 credentials=credentials, data=data).connect()
             else:
                 raise CompassException(content)
@@ -123,7 +125,8 @@ class Database(BaseObject):
         self.url = url
         self.name = name
         self.credentials = credentials
-        self.request = Request(username=credentials[0], password=credentials[1])
+        self.request = Request(username=credentials[0],
+                               password=credentials[1])
 
         if data is None:
             self.connect()
@@ -161,8 +164,7 @@ class Database(BaseObject):
             return Cluster(database=self, data=json.loads(content))
         else:
             raise CompassException(content)
-            
-        
+                    
     def klass(self, name, limit=20, create=False):
         if create:
             url = Klass.action['post'] % (self.url, self.name, name)
@@ -179,10 +181,10 @@ class Database(BaseObject):
             if response.status == 200:
                 data = json.loads(content)
 
-                return Klass(database=self, name=name, documents=data['result'])
+                return Klass(database=self, name=name, 
+                             documents=data['result'])
             else: 
                 raise CompassException(content)
-            
         
     def query(self, query):
         query = urllib.quote(query)
@@ -196,7 +198,6 @@ class Database(BaseObject):
         else:
             raise CompassException(content)
             
-        
     def document(self, rid=None, class_name=None, **data):
         if rid:
             url = Document.action['get'] % (self.url, self.name, rid)
@@ -217,8 +218,7 @@ class Database(BaseObject):
                 return self.document(rid=content, database=self)
             else:
                 raise CompassException(content)
-                
-        
+    
 
 class Cluster(BaseObject):
     action = {
@@ -281,8 +281,7 @@ class Klass(BaseObject):
                                         KlassProperty(name=prop['name'],
                                                       klass=self, data=prop)
                                        })
-                
-                
+
 
 class KlassProperty(BaseObject):
     action = {
@@ -347,7 +346,7 @@ class Document(BaseObject):
         
     def save(self):
         url = self.action['put'] % (self.db_url, self.db_name)
-        response, content = self.db_request.put(url=url, data= self.data)
+        response, content = self.db_request.put(url=url, data=self.data)
 
         if response.status == 200:
             return self
@@ -377,5 +376,3 @@ class Document(BaseObject):
         
         if response.status != 204:
             raise CompassException(content)
-        
-        
